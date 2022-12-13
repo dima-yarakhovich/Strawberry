08b214454c80f48624187ce2a93f6566bcb748a1
dima-yarakhovich <dima020490@tut.by>
2022-12-14 00:04:47 +0300

    Initial commit

---
+++ src/main/java/pro/sky/strawberry/StrawberryApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.strawberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawberryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrawberryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/strawberry/StrawberryApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.strawberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawberryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
